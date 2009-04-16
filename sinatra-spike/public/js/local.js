29ccc81f527d0dc174f8943a64f80d5f851711d0
Francesco Lazzarino <flazzarino@gmail.com>
2009-04-16 12:30:39 -0400

    more options ...

--- sinatra-spike/public/js/local.js
+++ sinatra-spike/public/js/local.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
   $("#tabs").tabs();
 
+  // submit buttons
   $("button[type='submit']").hover(
     function(){$(this).addClass("ui-state-hover"); },
     function(){$(this).removeClass("ui-state-hover"); }
-  );
-
-  $(".fg-button:not(.ui-state-disabled)").mousedown(function(){
-
+  ).mousedown(function(){
     $(this).parents('.fg-buttonset-single:first').find(".fg-button.ui-state-active").removeClass("ui-state-active");
     if( $(this).is('.ui-state-active.fg-button-toggleable, .fg-buttonset-multi .ui-state-active') ){
       $(this).removeClass("ui-state-active");
@@ -15,8 +13,19 @@ $(document).ready(function() {
       $(this).addClass("ui-state-active");
     }
   }).mouseup(function(){
-    if(! $(this).is('.fg-button-toggleable, .fg-buttonset-single .fg-button,  .fg-buttonset-multi .fg-button') ){
-        $(this).removeClass("ui-state-active");
-      }
+    if(! $(this).is('.fg-button-toggleable, .fg-buttonset-single .fg-button, .fg-buttonset-multi .fg-button') ){
+      $(this).removeClass("ui-state-active");
+    }
   });
+
+  // more options accordion
+  $(".moreoptions").accordion({
+    collapsible: true,
+    active: false
+  });
+
+  $('.accordion .head').click(function() {
+    $(this).next().toggle('slow');
+    return false;
+  }).next().hide();
 });
