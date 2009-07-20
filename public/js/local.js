cf9296557e95fce8f1d52c3479be583085138212
Francesco Lazzarino <flazzarino@gmail.com>
2009-07-20 18:03:45 -0400

    js is working!

--- public/js/local.js
+++ public/js/local.js
@@ -2,19 +2,39 @@ $(document).ready(function() {
 
 	$("#inputchoice a").click(function() {
 		
-		// deactivate all choices & hide forms
-		$("#inputchoice a").removeClass("active");
-		$("#input-uri").hide();
-		$("#input-file").hide();
-		$("#input-direct").hide();
-				
-		// show my form & activate my choice
-		$(this).addClass("active");
-		switch ($(this).attr("id")) {
-			case "choice-uri": $("#input-uri").show(); break;
-			case "choice-file": $("#input-file").show(); break;
-			case "choice-direct": $("#input-direct").show(); break;
-		}
-	});
+		var old_choice = $("#inputchoice a.active");
+		var old_pane = choice_to_pane(old_choice);
+		
+		var new_choice = $(this);
+		var new_pane = choice_to_pane(new_choice);
 
+		new_choice.animate( { color: '#9BBE00' }, 500).addClass("active");
+		old_choice.animate( { color: '#797B7A' }, 500).removeClass("active");
+
+		old_pane.fadeOut("fast", function () {
+			new_pane.fadeIn("fast");
+		});
+				
+	});
 });
+
+function choice_to_pane(choice) {
+
+	var choice_id = choice.attr("id");
+	var pane_id;
+	switch (choice_id) {
+		case "choice-uri": 
+		pane_id = "#input-uri"; 
+		break;
+		
+		case "choice-file": 
+		pane_id = "#input-file"; 
+		break;
+		
+		case "choice-direct": 
+		pane_id = "#input-direct"; 
+		break;
+	}
+	
+	return $(pane_id);
+}
